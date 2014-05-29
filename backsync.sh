14423523a180a42a6bdb7f94454463ca19a67021
Sam T <sthompson@sam-www>
2014-05-29 11:42:39 -0400

    more, better workin' ;)

--- backsync.sh
+++ backsync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
+drush vset -y file_temporary_path '/tmp'
 drush updb -y
 drush fra -y
--- golocal.sh
+++ golocal.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
-drush dis update -y
-drush dis acquia_agent -y
+drush vset -y file_temporary_path '/tmp'
+drush dis update acquia_agent apachesolr -y
 drush en devel -y
 drush vset error_level 1
 drush vset -y preprocess_css 0
 drush vset -y preprocess_js 0
 drush vset -y devel_xhprof_directory '/usr/share/php'
 drush vset -y devel_xhprof_url 'http://sam-www/xhprof_html'
-drush vset -y file_temporary_path '/tmp'
+drush vset -y views_skip_cache 1
